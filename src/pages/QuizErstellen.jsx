899928b21065e2c16df91096552ae59203179873
Rafael Switala <RafaelSwitala@onlineger.online-gmbh.de>
2024-11-06 23:09:56 +0100

    Beim Erstellen des Quizes ist es jetzt möglich, von Seite 2, 3 und 4 aus, zurück zu gehen zur vorherigen Seite

--- src/pages/QuizErstellen.jsx
+++ src/pages/QuizErstellen.jsx
@@ -152,202 +152,213 @@ const QuizErstellen = ({ show, onHide }) => {
 
   return (
     <Modal
-      className='quizModal'
-      show={show}
-      onHide={onHide}
-      size="xl"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header className='modalHeader' closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Neues Quiz Erstellen
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body className='modalBody'>
-        {currentPage === 1 && (
-          <>
-            <Form>
-              <Form.Group controlId="formQuizName">
-                <Form.Label className='modalText'>Name des Quizzes</Form.Label>
-                <Form.Control 
-                  className={`modalInput ${isQuiznameValid(quizName) ? '' : 'is-invalid'}`}
-                  type="text" 
-                  placeholder="Gib den Namen des Quizzes ein"
-                  value={quizName}
-                  onChange={(e) => setQuizName(e.target.value)} 
-                />
-                { !isQuiznameValid(quizName) && <p className="error-text">Ungültiger Quizname!</p> }
-              </Form.Group>
-            </Form>
+  className='quizModal'
+  show={show}
+  onHide={onHide}
+  size="xl"
+  aria-labelledby="contained-modal-title-vcenter"
+  centered
+>
+  <Modal.Header className='modalHeader' closeButton>
+    <Modal.Title id="contained-modal-title-vcenter">
+      Neues Quiz Erstellen
+    </Modal.Title>
+  </Modal.Header>
+  <Modal.Body className='modalBody'>
+    {currentPage === 1 && (
+      <>
+        <Form>
+          <Form.Group controlId="formQuizName">
+            <Form.Label className='modalText'>Name des Quizzes</Form.Label>
+            <Form.Control 
+              className={`modalInput ${isQuiznameValid(quizName) ? '' : 'is-invalid'}`}
+              type="text" 
+              placeholder="Gib den Namen des Quizzes ein"
+              value={quizName}
+              onChange={(e) => setQuizName(e.target.value)} 
+            />
+            { !isQuiznameValid(quizName) && <p className="error-text">Ungültiger Quizname!</p> }
+          </Form.Group>
+        </Form>
 
-            <h4 className='modalText'>Einstellungen</h4>
-            <div className="quiz-settings numberCategories">
-              <p className="settings-label-h1">Anzahl der Kategorien: {categoryCount}</p>
-              <input
-                className="settings-slider"
-                type="range"
-                min="2"
-                max="8"
-                value={categoryCount}
-                onChange={handleCategoryCountChange}
+        <h4 className='modalText'>Einstellungen</h4>
+        <div className="quiz-settings numberCategories">
+          <p className="settings-label-h1">Anzahl der Kategorien: {categoryCount}</p>
+          <input
+            className="settings-slider"
+            type="range"
+            min="2"
+            max="8"
+            value={categoryCount}
+            onChange={handleCategoryCountChange}
+          />
+        </div>
+
+        <div className="quiz-settings difficultyCategories">
+          <p className="settings-label-h1">Schwierigkeitslevel pro Kategorie: {rowCount}</p>
+          <input
+            className="settings-slider"
+            type="range"
+            min="3"
+            max="10"
+            value={rowCount}
+            onChange={handleRowCountChange}
+          />
+        </div>
+
+        <div className="quiz-settings pointSteps">
+          <h4 className='modalText'>Punkteschritte</h4>
+          <Form.Select
+            className='modalInput'
+            value={pointStep}
+            onChange={handlePointStepChange}
+          >
+            <option value={1}>1er Schritte</option>
+            <option value={10}>10er Schritte</option>
+            <option value={20}>20er Schritte</option>
+            <option value={50}>50er Schritte</option>
+            <option value={100}>100er Schritte</option>
+            <option value={200}>200er Schritte</option>
+            <option value={500}>500er Schritte</option>
+            <option value={1000}>1000er Schritte</option>
+          </Form.Select>
+        </div>
+
+        <Button className="button-1" onClick={generateQuestions}>
+          Fragenfelder generieren
+        </Button>
+      </>
+    )}
+
+    {currentPage === 2 && (
+      <>
+        <div className="category-inputs">
+          <h4>Kategorien eingeben:</h4>
+          {categories.map((category, index) => (
+            <Form.Group key={index} controlId={`category-${index}`}>
+              <Form.Label>Kategorie {index + 1}</Form.Label>
+              <Form.Control 
+                type="text"
+                placeholder={`Kategorie ${index + 1} eingeben`}
+                value={category}
+                onChange={(e) => handleCategoryChange(index, e.target.value)}
               />
-            </div>
+            </Form.Group>
+          ))}
+        </div>
 
-            <div className="quiz-settings difficultyCategories">
-              <p className="settings-label-h1">Schwierigkeitslevel pro Kategorie: {rowCount}</p>
-              <input
-                className="settings-slider"
-                type="range"
-                min="3"
-                max="10"
-                value={rowCount}
-                onChange={handleRowCountChange}
-              />
-            </div>
-
-            <div className="quiz-settings pointSteps">
-              <h4 className='modalText'>Punkteschritte</h4>
-              <Form.Select
-                className='modalInput'
-                value={pointStep}
-                onChange={handlePointStepChange}
-              >
-                <option value={1}>1er Schritte</option>
-                <option value={10}>10er Schritte</option>
-                <option value={20}>20er Schritte</option>
-                <option value={50}>50er Schritte</option>
-                <option value={100}>100er Schritte</option>
-                <option value={200}>200er Schritte</option>
-                <option value={500}>500er Schritte</option>
-                <option value={1000}>1000er Schritte</option>
-              </Form.Select>
-            </div>
-
-            <Button className="button-1" onClick={generateQuestions}>
-              Fragenfelder generieren
-            </Button>
-          </>
-        )}
-
-        {currentPage === 2 && (
-          <>
-            <div className="category-inputs">
-              <h4>Kategorien eingeben:</h4>
-              {categories.map((category, index) => (
-                <Form.Group key={index} controlId={`category-${index}`}>
-                  <Form.Label>Kategorie {index + 1}</Form.Label>
-                  <Form.Control 
-                    type="text"
-                    placeholder={`Kategorie ${index + 1} eingeben`}
-                    value={category}
-                    onChange={(e) => handleCategoryChange(index, e.target.value)}
-                  />
-                </Form.Group>
-              ))}
-            </div>
-
-            <div className="question-container">
-              {categories.map((category, catIndex) => (
-                <div key={catIndex}>
-                  <h4 className="category-title">Kategorie: {category || `Kategorie ${catIndex + 1}`}</h4>
-                  {questions
-                    .filter((q) => q.categoryIndex === catIndex)
-                    .map((question) => (
-                      <div key={question.id} className="question-item">
-                        <h5>Frage für {question.points} Punkte</h5>
-                        <textarea
-                          className="question-input"
-                          type="text"
-                          placeholder="Frage eingeben"
-                          value={question.question}
-                          onChange={(e) => handleQuestionChange(question.id, 'question', e.target.value)}
-                        />
-                        <input
-                          className="answer-input"
-                          type="text"
-                          placeholder="Antwort eingeben"
-                          value={question.answer}
-                          onChange={(e) => handleQuestionChange(question.id, 'answer', e.target.value)}
-                        />
-                        <div className="options-container">
+        <div className="question-container">
+          {categories.map((category, catIndex) => (
+            <div key={catIndex}>
+              <h4 className="category-title">Kategorie: {category || `Kategorie ${catIndex + 1}`}</h4>
+              {questions
+                .filter((q) => q.categoryIndex === catIndex)
+                .map((question) => (
+                  <div key={question.id} className="question-item">
+                    <h5>Frage für {question.points} Punkte</h5>
+                    <textarea
+                      className="question-input"
+                      type="text"
+                      placeholder="Frage eingeben"
+                      value={question.question}
+                      onChange={(e) => handleQuestionChange(question.id, 'question', e.target.value)}
+                    />
+                    <input
+                      className="answer-input"
+                      type="text"
+                      placeholder="Antwort eingeben"
+                      value={question.answer}
+                      onChange={(e) => handleQuestionChange(question.id, 'answer', e.target.value)}
+                    />
+                    <div className="options-container">
+                      <Button
+                        className="button-secondary"
+                        onClick={() => addOptionHandler(question.id)}
+                      >
+                        Antwortoption hinzufügen
+                      </Button>
+                      {question.options.map((option, index) => (
+                        <div key={index} className="option-item">
+                          <input
+                            className="option-input"
+                            type="text"
+                            placeholder={`Option ${index + 1}`}
+                            value={option}
+                            onChange={(e) => handleOptionChange(question.id, index, e.target.value)}
+                          />
                           <Button
-                            className="button-secondary"
-                            onClick={() => addOptionHandler(question.id)}
+                            className="button-danger"
+                            onClick={() => removeOptionHandler(question.id, index)}
                           >
-                            Antwortoption hinzufügen
+                            Entfernen
                           </Button>
-                          {question.options.map((option, index) => (
-                            <div key={index} className="option-item">
-                              <input
-                                className="option-input"
-                                type="text"
-                                placeholder={`Option ${index + 1}`}
-                                value={option}
-                                onChange={(e) => handleOptionChange(question.id, index, e.target.value)}
-                              />
-                              <Button
-                                className="button-danger"
-                                onClick={() => removeOptionHandler(question.id, index)}
-                              >
-                                Entfernen
-                              </Button>
-                            </div>
-                          ))}
                         </div>
-                      </div>
-                    ))}
-                </div>
-              ))}
+                      ))}
+                    </div>
+                  </div>
+                ))}
             </div>
+          ))}
+        </div>
 
-            <Button onClick={() => setCurrentPage(3)}>Weiter zu Seite 3</Button>
-          </>
-        )}
-        {currentPage === 3 && (
-          <>
-            {/* Seite 3: Weitere Einstellungen */}
-            <Form.Group controlId="timer">
-              <Form.Label>Timer-Dauer:</Form.Label>
-              <Form.Select value={timer} onChange={(e) => setTimer(Number(e.target.value))}>
-                <option value={15}>15 Sekunden</option>
-                <option value={30}>30 Sekunden</option>
-                <option value={45}>45 Sekunden</option>
-                <option value={60}>1 Minute</option>
-              </Form.Select>
-            </Form.Group>
+        <Button onClick={() => setCurrentPage(3)}>Weiter zu Seite 3</Button>
+        <Button className="button-secondary" onClick={() => setCurrentPage(1)}>
+          Zurück zu Seite 1
+        </Button>
+      </>
+    )}
 
-            <Form.Group controlId="incorrectAnswerBehavior">
-              <Form.Label>Verhalten bei falscher Antwort:</Form.Label>
-              <Form.Select value={incorrectAnswerBehavior} onChange={(e) => setIncorrectAnswerBehavior(e.target.value)}>
-                <option value="skip">Keine Aktion - Frage wird geschlossen</option>
-                <option value="retry">Erneut versuchen - 2. Versuch</option>
-                <option value="minus">Minuspunkte - Punktezahl wird abziehen</option>
-                <option value="buzzer">Buzzer - Andere Spieler dürfen antworten</option>
-              </Form.Select>
-            </Form.Group>
+    {currentPage === 3 && (
+      <>
+        {/* Seite 3: Weitere Einstellungen */}
+        <Form.Group controlId="timer">
+          <Form.Label>Timer-Dauer:</Form.Label>
+          <Form.Select value={timer} onChange={(e) => setTimer(Number(e.target.value))}>
+            <option value={15}>15 Sekunden</option>
+            <option value={30}>30 Sekunden</option>
+            <option value={45}>45 Sekunden</option>
+            <option value={60}>1 Minute</option>
+          </Form.Select>
+        </Form.Group>
 
-            <Form.Group controlId="openOptionsBehavior">
-              <Form.Label>Verhalten beim Öffnen der Antwortmöglichkeiten</Form.Label>
-              <Form.Select value={openOptionsBehavior} onChange={(e) => setOpenOptionsBehavior(e.target.value)}>
-                <option value="full">Volle Punktzahl bleibt erreichbar</option>
-                <option value="half">Die Punktzahl halbiert sich</option>
-              </Form.Select>
-            </Form.Group>
-            
-            <Button onClick={handleCreateJson}>Quiz erstellen</Button>
-          </>
-        )}
+        <Form.Group controlId="incorrectAnswerBehavior">
+          <Form.Label>Verhalten bei falscher Antwort:</Form.Label>
+          <Form.Select value={incorrectAnswerBehavior} onChange={(e) => setIncorrectAnswerBehavior(e.target.value)}>
+            <option value="skip">Keine Aktion - Frage wird geschlossen</option>
+            <option value="retry">Erneut versuchen - 2. Versuch</option>
+            <option value="minus">Minuspunkte - Punktezahl wird abziehen</option>
+            <option value="buzzer">Buzzer - Andere Spieler dürfen antworten</option>
+          </Form.Select>
+        </Form.Group>
 
-        {currentPage === 4 && (
-          <>
-            {/* Seite 4: Status der Erstellung */}
-            <h4>{creationStatus}</h4>
-            <Button onClick={onHide}>Schließen</Button>
-          </>
-        )}
-      </Modal.Body>
-    </Modal>
+        <Form.Group controlId="openOptionsBehavior">
+          <Form.Label>Verhalten beim Öffnen der Antwortmöglichkeiten</Form.Label>
+          <Form.Select value={openOptionsBehavior} onChange={(e) => setOpenOptionsBehavior(e.target.value)}>
+            <option value="full">Volle Punktzahl bleibt erreichbar</option>
+            <option value="half">Die Punktzahl halbiert sich</option>
+          </Form.Select>
+        </Form.Group>
+
+        <Button onClick={handleCreateJson}>Quiz erstellen</Button>
+        <Button className="button-secondary" onClick={() => setCurrentPage(2)}>
+          Zurück zu Seite 2
+        </Button>
+      </>
+    )}
+
+    {currentPage === 4 && (
+      <>
+        {/* Seite 4: Status der Erstellung */}
+        <h4>{creationStatus}</h4>
+        <Button onClick={onHide}>Schließen</Button>
+        <Button className="button-secondary" onClick={() => setCurrentPage(3)}>
+          Zurück zu Seite 3
+        </Button>
+      </>
+    )}
+  </Modal.Body>
+</Modal>
+
   );
 };
 
